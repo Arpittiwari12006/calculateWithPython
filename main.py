8e1d58130f1ab6bd5155757f601df6433a126458
Arpittiwari12006 <arpittiwari12006@gmail.com>
2025-12-31 22:15:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('Hello')
+import math
+
+print("calculateWithPython")
+print("Use math functions directly or using math.")
+print("WANT TO QUIT TYPE exit ")
+
+while True:
+    expr = input(">>> ")
+
+    if expr.lower() == "exit":
+        print("Calculator Closed")
+        break
+
+    try:
+        result = eval(expr)
+        print("=", result)
+    except:
+        print("Invalid Expression")
